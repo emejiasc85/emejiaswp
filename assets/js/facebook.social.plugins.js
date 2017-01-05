5c6b07ddfbdf557090a9eabd784d12e9755237bd
emejiasc85 <emejiasc85@gmail.com>
2017-01-05 16:22:08 -0600

    fix url on fb plugin

--- assets/js/facebook.social.plugins.js
+++ assets/js/facebook.social.plugins.js
@@ -1,9 +1,16 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '606064619576794',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+  };
 
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.8";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
